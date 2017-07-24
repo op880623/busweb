cb072b0b568f41002c5a133d24773fe5a13ef43e
op880623 <op880623@gmail.com>
2017-07-24 16:51:17 +0800

    modified setting

--- busweb/settings.py
+++ busweb/settings.py
@@ -29,6 +29,7 @@ DEBUG = False
 # Application definition
 
 INSTALLED_APPS = [
+    'bus.apps.BusConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
