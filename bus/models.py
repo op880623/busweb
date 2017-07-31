119348fdfe1fe98cf92446d558e66e252bab1415
op880623 <op880623@gmail.com>
2017-07-31 17:05:15 +0800

    add 2 functions to BusStop and their related tests and comments of multiple funnctions

--- bus/models.py
+++ bus/models.py
@@ -11,6 +11,32 @@ class BusStop(models.Model):
     def __str__(self):
         return self.name
 
+    def stops_can_go(self):
+        # return array of dicts that contain stop which self can go and routes from self to stop
+        # one stop and a set of route in a dict
+        # a series of dicts in a array
+        # ruturn format [{'stop': BusStop, 'route': <QuerySet of BusRoute>}*n]
+        stopsCanGo = BusStop.objects.none()
+        for route in self.route.all():
+            stopsCanGo = stopsCanGo.union(route.stops_after_specific_stop(self, order=False))
+        stopsHowToGo = []
+        for stop in stopsCanGo:
+            stopsHowToGo.append({'stop': stop,'route': self.route.all().intersection(stop.route.all())})
+        return stopsHowToGo
+
+    def stops_can_come(self):
+        # return array of dicts that contain stop which can come to self and routes from stop to self
+        # one stop and a set of route in a dict
+        # a series of dicts in a array
+        # ruturn format [{'stop': BusStop, 'route': <QuerySet of BusRoute>}*n]
+        stopsCanCome = BusStop.objects.none()
+        for route in self.route.all():
+            stopsCanCome = stopsCanCome.union(route.stops_before_specific_stop(self, order=False))
+        stopsHowToCome = []
+        for stop in stopsCanCome:
+            stopsHowToCome.append({'stop': stop,'route': self.route.all().intersection(stop.route.all())})
+        return stopsHowToCome
+
 
 class BusRoute(models.Model):
     uid = models.CharField(max_length=15)# id used in target website
@@ -20,7 +46,11 @@ class BusRoute(models.Model):
     def __str__(self):
         return self.name
 
-    def stops_after_specific_stop(self, stop):
+    def stops_after_specific_stop(self, stop, order=True):
+        # input stop can be BusStop, BusStop.id(str) or BusStop.name(str)
+        # ruturn a QuerySet containing BusStops after given BusStop on this BusRoute
+        # result is sorted by order on this BusRoute by default
+        # set order to False to get result which is not sorted
         if isinstance(stop, BusStop):
             pass
         elif isinstance(stop, str):
@@ -37,9 +67,16 @@ class BusRoute(models.Model):
             stopInfo = StopOnRoute.objects.get(route=self, stop=stop)
         except:
             raise ValueError('StopOnRoute matching this uid or name does not exist.')
-        return self.stops.filter(stoponroute__direction=stopInfo.direction, stoponroute__order__gt=stopInfo.order).order_by('stoponroute__order')
+        if order is True:
+            return self.stops.filter(stoponroute__direction=stopInfo.direction, stoponroute__order__gt=stopInfo.order).order_by('stoponroute__order')
+        else:
+            return self.stops.filter(stoponroute__direction=stopInfo.direction, stoponroute__order__gt=stopInfo.order)
 
-    def stops_before_specific_stop(self, stop):
+    def stops_before_specific_stop(self, stop, order=True):
+        # input stop can be BusStop, BusStop.id(str) or BusStop.name(str)
+        # ruturn a QuerySet containing BusStops before given BusStop on this BusRoute
+        # result is sorted by order on this BusRoute by default
+        # set order to False to get result which is not sorted
         if isinstance(stop, BusStop):
             pass
         elif isinstance(stop, str):
@@ -56,7 +93,10 @@ class BusRoute(models.Model):
             stopInfo = StopOnRoute.objects.get(route=self, stop=stop)
         except:
             raise ValueError('StopOnRoute matching this uid or name does not exist.')
-        return self.stops.filter(stoponroute__direction=stopInfo.direction, stoponroute__order__lt=stopInfo.order).order_by('stoponroute__order')
+        if order is True:
+            return self.stops.filter(stoponroute__direction=stopInfo.direction, stoponroute__order__lt=stopInfo.order).order_by('stoponroute__order')
+        else:
+            return self.stops.filter(stoponroute__direction=stopInfo.direction, stoponroute__order__lt=stopInfo.order)
 
 
 class StopOnRoute(models.Model):
--- bus/tests.py
+++ bus/tests.py
@@ -96,6 +96,7 @@ class TestBus(TestCase):
 class TestBusBasic(TestBus):
 
     def test_setup_name_and_uid(self):
+        # check name and id are right
         self.assertEqual(self.r01.uid, '01')
         self.assertEqual(self.r01.name, 'r01')
         self.assertEqual(self.r02.uid, '02')
@@ -128,6 +129,7 @@ class TestBusBasic(TestBus):
         self.assertEqual(self.s12.name, 's12')
 
     def test_route_stops(self):
+        # check BusRoute.stops can work
         ans = []
         for stop in self.r01.stops.filter(stoponroute__direction=True).order_by('stoponroute__order'):
             ans.append(stop.name)
@@ -159,6 +161,7 @@ class TestBusBasic(TestBus):
         self.assertEqual('-'.join(ans), 's05-s06-s07-s08')
 
     def test_stop_routes(self):
+        # check BusStop.route can work
         self.assertIn(self.r01, self.s01.route.all())
         self.assertIn(self.r01, self.s02.route.all())
         self.assertIn(self.r01, self.s03.route.all())
@@ -200,6 +203,7 @@ class TestBusBasic(TestBus):
 class TestBusRouteStopsAfterSpecificStop(TestBus):
 
     def test_with_stop_input(self):
+        # test function can operate with BusStop as input
         self.assertEqual('-'.join([stop.name for stop in self.r01.stops_after_specific_stop(self.s01)]), 's02-s03-s04')
         self.assertEqual('-'.join([stop.name for stop in self.r01.stops_after_specific_stop(self.s02)]), 's03-s04')
         self.assertEqual('-'.join([stop.name for stop in self.r01.stops_after_specific_stop(self.s03)]), 's04')
@@ -210,6 +214,7 @@ class TestBusRouteStopsAfterSpecificStop(TestBus):
         self.assertEqual('-'.join([stop.name for stop in self.r01.stops_after_specific_stop(self.s12)]), '')
 
     def test_with_stop_id_input(self):
+        # test function can operate with BusStop.id as input
         self.assertEqual('-'.join([stop.name for stop in self.r01.stops_after_specific_stop(self.s01.uid)]), 's02-s03-s04')
         self.assertEqual('-'.join([stop.name for stop in self.r01.stops_after_specific_stop(self.s02.uid)]), 's03-s04')
         self.assertEqual('-'.join([stop.name for stop in self.r01.stops_after_specific_stop(self.s03.uid)]), 's04')
@@ -220,6 +225,7 @@ class TestBusRouteStopsAfterSpecificStop(TestBus):
         self.assertEqual('-'.join([stop.name for stop in self.r01.stops_after_specific_stop(self.s12.uid)]), '')
 
     def test_with_stop_name_input(self):
+        # test function can operate with BusStop.name as input
         self.assertEqual('-'.join([stop.name for stop in self.r01.stops_after_specific_stop(self.s01.name)]), 's02-s03-s04')
         self.assertEqual('-'.join([stop.name for stop in self.r01.stops_after_specific_stop(self.s02.name)]), 's03-s04')
         self.assertEqual('-'.join([stop.name for stop in self.r01.stops_after_specific_stop(self.s03.name)]), 's04')
@@ -247,6 +253,7 @@ class TestBusRouteStopsAfterSpecificStop(TestBus):
 class TestBusRouteStopsBeforeSpecificStop(TestBus):
 
     def test_with_stop_input(self):
+        # test function can operate with BusStop as input
         self.assertEqual('-'.join([stop.name for stop in self.r01.stops_before_specific_stop(self.s01)]), '')
         self.assertEqual('-'.join([stop.name for stop in self.r01.stops_before_specific_stop(self.s02)]), 's01')
         self.assertEqual('-'.join([stop.name for stop in self.r01.stops_before_specific_stop(self.s03)]), 's01-s02')
@@ -257,6 +264,7 @@ class TestBusRouteStopsBeforeSpecificStop(TestBus):
         self.assertEqual('-'.join([stop.name for stop in self.r01.stops_before_specific_stop(self.s12)]), 's05-s06-s11')
 
     def test_with_stop_id_input(self):
+        # test function can operate with BusStop.id as input
         self.assertEqual('-'.join([stop.name for stop in self.r01.stops_before_specific_stop(self.s01.uid)]), '')
         self.assertEqual('-'.join([stop.name for stop in self.r01.stops_before_specific_stop(self.s02.uid)]), 's01')
         self.assertEqual('-'.join([stop.name for stop in self.r01.stops_before_specific_stop(self.s03.uid)]), 's01-s02')
@@ -267,6 +275,7 @@ class TestBusRouteStopsBeforeSpecificStop(TestBus):
         self.assertEqual('-'.join([stop.name for stop in self.r01.stops_before_specific_stop(self.s12.uid)]), 's05-s06-s11')
 
     def test_with_stop_name_input(self):
+        # test function can operate with BusStop.name as input
         self.assertEqual('-'.join([stop.name for stop in self.r01.stops_before_specific_stop(self.s01.name)]), '')
         self.assertEqual('-'.join([stop.name for stop in self.r01.stops_before_specific_stop(self.s02.name)]), 's01')
         self.assertEqual('-'.join([stop.name for stop in self.r01.stops_before_specific_stop(self.s03.name)]), 's01-s02')
@@ -289,3 +298,52 @@ class TestBusRouteStopsBeforeSpecificStop(TestBus):
         # test with StopOnRoute does not exist
         with self.assertRaisesRegex(ValueError, "StopOnRoute matching this uid or name does not exist."):
             self.r01.stops_before_specific_stop(self.s07)
+
+
+class TestBusStopStopsCanGo(TestBus):
+
+    def test_stop_should_in_result(self):
+        # confirm number of result is right
+        self.assertEqual(len(self.s01.stops_can_go()), 5)
+        self.assertEqual(len(self.s02.stops_can_go()), 4)
+        self.assertEqual(len(self.s03.stops_can_go()), 1)
+        self.assertEqual(len(self.s04.stops_can_go()), 0)
+        self.assertEqual(len(self.s05.stops_can_go()), 5)
+        self.assertEqual(len(self.s06.stops_can_go()), 4)
+        self.assertEqual(len(self.s07.stops_can_go()), 1)
+        self.assertEqual(len(self.s08.stops_can_go()), 0)
+        self.assertEqual(len(self.s09.stops_can_go()), 5)
+        self.assertEqual(len(self.s10.stops_can_go()), 4)
+        self.assertEqual(len(self.s11.stops_can_go()), 1)
+        self.assertEqual(len(self.s12.stops_can_go()), 0)
+        # confirm instances should in result are in result
+        self.assertIn(self.s02, [stop['stop'] for stop in self.s01.stops_can_go()])
+        self.assertIn(self.s03, [stop['stop'] for stop in self.s01.stops_can_go()])
+        self.assertIn(self.s04, [stop['stop'] for stop in self.s01.stops_can_go()])
+        self.assertIn(self.s07, [stop['stop'] for stop in self.s01.stops_can_go()])
+        self.assertIn(self.s08, [stop['stop'] for stop in self.s01.stops_can_go()])
+        self.assertIn(self.s03, [stop['stop'] for stop in self.s02.stops_can_go()])
+        self.assertIn(self.s04, [stop['stop'] for stop in self.s02.stops_can_go()])
+        self.assertIn(self.s07, [stop['stop'] for stop in self.s02.stops_can_go()])
+        self.assertIn(self.s08, [stop['stop'] for stop in self.s02.stops_can_go()])
+        self.assertIn(self.s04, [stop['stop'] for stop in self.s03.stops_can_go()])
+        self.assertIn(self.s06, [stop['stop'] for stop in self.s05.stops_can_go()])
+        self.assertIn(self.s07, [stop['stop'] for stop in self.s05.stops_can_go()])
+        self.assertIn(self.s08, [stop['stop'] for stop in self.s05.stops_can_go()])
+        self.assertIn(self.s11, [stop['stop'] for stop in self.s05.stops_can_go()])
+        self.assertIn(self.s12, [stop['stop'] for stop in self.s05.stops_can_go()])
+        self.assertIn(self.s07, [stop['stop'] for stop in self.s06.stops_can_go()])
+        self.assertIn(self.s08, [stop['stop'] for stop in self.s06.stops_can_go()])
+        self.assertIn(self.s11, [stop['stop'] for stop in self.s06.stops_can_go()])
+        self.assertIn(self.s12, [stop['stop'] for stop in self.s06.stops_can_go()])
+        self.assertIn(self.s08, [stop['stop'] for stop in self.s07.stops_can_go()])
+        self.assertIn(self.s03, [stop['stop'] for stop in self.s09.stops_can_go()])
+        self.assertIn(self.s04, [stop['stop'] for stop in self.s09.stops_can_go()])
+        self.assertIn(self.s10, [stop['stop'] for stop in self.s09.stops_can_go()])
+        self.assertIn(self.s11, [stop['stop'] for stop in self.s09.stops_can_go()])
+        self.assertIn(self.s12, [stop['stop'] for stop in self.s09.stops_can_go()])
+        self.assertIn(self.s03, [stop['stop'] for stop in self.s10.stops_can_go()])
+        self.assertIn(self.s04, [stop['stop'] for stop in self.s10.stops_can_go()])
+        self.assertIn(self.s11, [stop['stop'] for stop in self.s10.stops_can_go()])
+        self.assertIn(self.s12, [stop['stop'] for stop in self.s10.stops_can_go()])
+        self.assertIn(self.s12, [stop['stop'] for stop in self.s11.stops_can_go()])
