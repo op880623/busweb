d9a08bc918fd08311f56ddc0ba7082fa083eec51
op880623 <op880623@gmail.com>
2018-03-19 16:34:45 +0800

    complete views

--- bus/views.py
+++ bus/views.py
@@ -1,19 +1,36 @@
+import json
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from bus.models import Bus, Stop
+
 
 def all_stops(request):
-    json = ''
-    return HttpResponse(json)
+    data = {}
+    data['stop'] = {}
+    for stop in Stop.objects.all():
+        data['stop'][stop.uid] = stop.to_hash()
+    data['route'] = {}
+    for bus in Bus.objects.all():
+        data['route'][bus.uid] = bus.name
+    return HttpResponse(json.dumps(data))
 
 def departure(request, uid):
-    json = ''
-    return HttpResponse(json)
+    stop = Stop.get(uid)
+    data = {}
+    data['departure'] = [s for s in stop.stops_can_go()]
+    return HttpResponse(json.dumps(data))
 
 def destination(request, uid):
-    json = ''
-    return HttpResponse(json)
+    stop = Stop.get(uid)
+    data = {}
+    data['destination'] = [s for s in stop.stops_can_come()]
+    return HttpResponse(json.dumps(data))
 
 def connected(request, uid):
-    json = ''
-    return HttpResponse(json)
+    stop = Stop.get(uid)
+    data = {}
+    data['departure'] = [s for s in stop.stops_can_go()]
+    data['destination'] = [s for s in stop.stops_can_come()]
+    return HttpResponse(json.dumps(data))
