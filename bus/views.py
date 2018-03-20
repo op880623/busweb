3136cb7abd7e4eec086fd4723fbcb06630446977
op880623 <op880623@gmail.com>
2018-03-20 13:41:36 +0800

    use JsonResponse

--- bus/views.py
+++ bus/views.py
@@ -1,36 +1,33 @@
-import json
-
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 from bus.models import Bus, Stop
 
 
 def all_stops(request):
-    data = {}
-    data['stop'] = {}
+    data = {
+        'stop': {},
+        'route': {}
+    }
     for stop in Stop.objects.all():
         data['stop'][stop.uid] = stop.to_hash()
-    data['route'] = {}
     for bus in Bus.objects.all():
         data['route'][bus.uid] = bus.name
-    return HttpResponse(json.dumps(data))
+    return JsonResponse(data)
 
 def departure(request, uid):
     stop = Stop.get(uid)
-    data = {}
-    data['departure'] = [s for s in stop.stops_can_go()]
-    return HttpResponse(json.dumps(data))
+    data = {'departure': [s for s in stop.stops_can_go()]}
+    return JsonResponse(data)
 
 def destination(request, uid):
     stop = Stop.get(uid)
-    data = {}
-    data['destination'] = [s for s in stop.stops_can_come()]
-    return HttpResponse(json.dumps(data))
+    data = {'destination': [s for s in stop.stops_can_come()]}
+    return JsonResponse(data)
 
 def connected(request, uid):
     stop = Stop.get(uid)
-    data = {}
-    data['departure'] = [s for s in stop.stops_can_go()]
-    data['destination'] = [s for s in stop.stops_can_come()]
-    return HttpResponse(json.dumps(data))
+    data = {
+        'departure': [s for s in stop.stops_can_go()],
+        'destination': [s for s in stop.stops_can_come()]
+    }
+    return JsonResponse(data)
